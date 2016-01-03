deb3343be53b3fbbc01edf7fa00f1f55b9447a2b
Saurabh Kaushik <saurabh.kaushik.in@gmail.com>
2016-01-03 22:09:45 +0530

    x

--- GooglePredictionApp/src/com/gpa/common/Constants.java
+++ GooglePredictionApp/src/com/gpa/common/Constants.java
@@ -1,15 +1,15 @@
 package com.gpa.common;
 
 public class Constants { 
-	public static String APPLICATION_NAME = "googpredictapp";
-	public static String PROJECT_NAME = "googpredictapp";
+	public static String APPLICATION_NAME = "";
+	public static String PROJECT_NAME = "";
 	
-	public static String GCP_ACCOUNT_KEY = "appdata/googpredictapp-4aeb82a0eaf6.p12";	
-	public static String GCP_ACCOUNT_ID = "superskpredict@googpredictapp.iam.gserviceaccount.com";
+	public static String GCP_ACCOUNT_KEY = "";	
+	public static String GCP_ACCOUNT_ID = "";
 
-	public static String GCP_STORAGE_BUCKET = "predictapp";
-	public static String GCP_STORAGE_TRAINING_FILENAME = "train.csv";
-	public static String GCP_STORAGE_TESTING_FILENAME = "test.csv";
+	public static String GCP_STORAGE_BUCKET = "";
+	public static String GCP_STORAGE_TRAINING_FILENAME = "";
+	public static String GCP_STORAGE_TESTING_FILENAME = "";
 	
 	public static String GCP_STORAGE_TRAINING_URL = GCP_STORAGE_BUCKET + "/" + GCP_STORAGE_TRAINING_FILENAME; 
 	public static String GCP_STORAGE_TESTING_URL  = GCP_STORAGE_BUCKET + "/" + GCP_STORAGE_TESTING_FILENAME;
